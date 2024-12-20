21b957aa47d9962c1f58b336000aee05d12b1f03
Julian <nortonjulian@gmail.com>
2024-12-20 09:53:54 -0700

    morning project

--- script.js
+++ script.js
@@ -13,36 +13,61 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
   };
   randomQuote();
-  
-  // Do all of your work inside the document.addEventListener  
+
+  // Do all of your work inside the document.addEventListener
 
   // Part 1
+const mainTitle = document.getElementById('main-title')
+mainTitle.textContent = "Welcome to DOM's Homepage!"
 
 
   // Part 2
+document.body.style.backgroundColor = "light blue"
+
 
 
   // Part 3
-
+const favThings = document.getElementById('favorite-things')
+favThings.lastElementChild.remove()
 
   // Part 4
-
+const specTitle = document.querySelectorAll('special-title')
+specTitle.forEach(title => {
+  title.style.fontsixe = '2rem'
+})
 
   // Part 5
-
-
+const pastRaces = document.getElementById('past-races')
+const races = pastRaces.querySelectorAll('li')
+races.forEach(race => {
+  if(race.textContent === "Chicago"){
+    race.remove()
+  }
+})
   // Part 6
-
+const newCity = document.createElement('li')
+newCity.textContent = "New York"
+pastRaces.appendChild(newCity)
 
   // Part 7
-
+const mainDiv = document.querySelector('.main')
+const newBlogPost = document.createElement('div')
+newBlogPost.className = "blog-post purple"
+newBlogPost.innerHTML = `<h1>New York</h1><p>I raced down Times Square in my car!</p>`;
+mainDiv.appendChild(newBlogPost)
 
   // Part 8
-
+const quoteTitle = document.getElementById('quote-title')
+quoteTitle.addEventListener('click', randomQuote)
 
   // Part 9
-
-
-
-
+const blogPosts = document.querySelectorAll("blog-post")
+blogPosts.forEach(post => {
+  post.addEventListener('mouseover', () => {
+    post.classList.toggle('red')
+  });
+  post.addEventListener('mouseenter', () => {
+    post.classList.toggle('purple')
+  })
+ })
 });
